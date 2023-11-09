e56f0cbc29bc52fae6bd38cbf54781c4943c923e
IvanMakarovDev <Biomoloko1@gmail.com>
2023-11-09 19:43:31 +0300

    OnHome

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,12 @@ public class Main {
         _moexDelta = MoexDelta();
         double _yearsAmount;
         int _yearsForCalc = 0;
-        int _initialCapital = 1;
-        float _getPercent = 0.05f;
+        int _initialCapital = 100;
+        float _getPercent = 0.5f;
         double _startYearCapital = _initialCapital - _getPercent; //первый стартовый капитал минус первый процент изъятия
         double _endYearCapital;
+
+        int iterations = 0;
 
         System.out.println("Paste Year:");
         Scanner yearsInput = new Scanner(System.in);
@@ -22,15 +24,16 @@ public class Main {
         _yearsAmount = 2022 - _yearsForCalc;
 
 
-        System.out.println("Начало : " + _startYearCapital);
-
+        //System.out.println("Начало " + _yearsForCalc +" " +_startYearCapital);
         for (double i = _yearsForCalc; i < 2022; i++) {
+
+
              _endYearCapital = _startYearCapital + (_startYearCapital * _moexDelta[(int)(2022f - i)]);
-            System.out.println("Конец : " + _endYearCapital);
-             _startYearCapital = _endYearCapital - (_getPercent + Constances.INFLATION_RATE[(int)(2022f - i)]);
-            System.out.println("Начало : " + _startYearCapital);
-
-
+            //System.out.println("         Конец " + i + " : " + _endYearCapital);
+             _startYearCapital = _endYearCapital - (_getPercent + Constances.INFLATION_RATE[(int)(iterations)]);
+            //System.out.println(" Начало " + i + " : " + _startYearCapital);
+            System.out.println("инфляция за " + i + " год " + Constances.INFLATION_RATE[iterations]);
+            iterations ++;
         }
     }
 
