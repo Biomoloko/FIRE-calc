6b789e59d3f22541788efbcb1cb4904e65c94543
IvanMakarovDev <Biomoloko1@gmail.com>
2023-11-14 04:12:47 +0300

    OnHome
    onMyWay

--- src/Main.java
+++ src/Main.java
@@ -9,32 +9,37 @@ public class Main {
         /*_moexDelta[0] = 0;*/
 
         _moexDelta = MoexDelta();
-        double _yearsAmount;
+        int _yearsAmount;
         int _yearsForCalc = 0;
         int _initialCapital = 100;
-        float _getPercent = 0.5f;
+        float _getPercent = 10f;
         double _startYearCapital = _initialCapital - _getPercent; //первый стартовый капитал минус первый процент изъятия
-        double _endYearCapital;
+        double _endYearCapital = 100;
+        boolean enough = false;
 
-        int iterations = 0;
+
 
         System.out.println("Paste Year:");
         Scanner yearsInput = new Scanner(System.in);
         _yearsForCalc = yearsInput.nextInt();
         _yearsAmount = 2022 - _yearsForCalc;
+        int _iterations = (Constances.INFLATION_RATE.length - _yearsAmount);
 
-
-        //System.out.println("Начало " + _yearsForCalc +" " +_startYearCapital);
-        for (double i = _yearsForCalc; i < 2022; i++) {
-
-
-             _endYearCapital = _startYearCapital + (_startYearCapital * _moexDelta[(int)(2022f - i)]);
-            //System.out.println("         Конец " + i + " : " + _endYearCapital);
-             _startYearCapital = _endYearCapital - (_getPercent + Constances.INFLATION_RATE[(int)(iterations)]);
-            //System.out.println(" Начало " + i + " : " + _startYearCapital);
-            System.out.println("инфляция за " + i + " год " + Constances.INFLATION_RATE[iterations]);
-            iterations ++;
+        System.out.println(" Начало " + _yearsForCalc + " : " + _startYearCapital);
+    while(_startYearCapital > 0) {
+            int counter = _iterations;
+        for (int i = _yearsForCalc; i < 2021; i++) {
+                _endYearCapital = _startYearCapital + (_startYearCapital * _moexDelta[counter]);
+                System.out.println("         Конец " + i + " : " + _endYearCapital);
+                _startYearCapital = _endYearCapital - (_getPercent + (Constances.INFLATION_RATE[counter-1]));
+                System.out.println(" Начало " + (i + 1) + " : " + _startYearCapital);
+            counter++;
         }
+        _getPercent += 0.5;
+        System.out.println(_getPercent);
+    }
+    _getPercent -= 0.5;
+        System.out.println(_getPercent);
     }
 
     //вычисление дельты мосБиржи
@@ -42,7 +47,7 @@ public class Main {
         double [] moexDelta = new double[21];
         moexDelta[0] = 0;
         for (int i = 1; i < 21; i++) {
-            moexDelta[i] = ((Constances.MOEX_RATE[i] - Constances.MOEX_RATE[i-1])/Constances.MOEX_RATE[i-1])*100;
+            moexDelta[i] = ((Constances.MOEX_RATE[i] - Constances.MOEX_RATE[i-1])/Constances.MOEX_RATE[i-1]);
         }
         return moexDelta;
     }
