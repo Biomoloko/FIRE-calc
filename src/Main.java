457ca5d3e291e47af274983b03a5299ef83d415e
MakarivIvanDev <Biomoloko1@gmail.com>
2023-11-16 03:13:45 +0300

    OnJob

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         double [] _moexDelta = new double[21];
         double [] _endingYearCapital;
-        /*_moexDelta[0] = 0;*/
 
         _moexDelta = MoexDelta();
         int _yearsAmount;
         int _yearsForCalc = 0;
         int _initialCapital = 100;
-        float _getPercent = 4f;
-        double _startYearCapital = _initialCapital - _getPercent; //первый стартовый капитал минус первый процент изъятия
-        double _endYearCapital = 100;
-        boolean enough = false;
+        float _getPercent = 0f;
+        double _startYearCapital = 100;//_initialCapital - _getPercent; //первый стартовый капитал минус первый процент изъятия
+        double _endYearCapital = 0;
 
 
 
@@ -23,25 +19,34 @@ public class Main {
         Scanner yearsInput = new Scanner(System.in);
         _yearsForCalc = yearsInput.nextInt();
         _yearsAmount = 2022 - _yearsForCalc;
-        int _iterations = (Constances.INFLATION_RATE.length - _yearsAmount);
-
-        System.out.println(" Начало " + _yearsForCalc + " : " + _startYearCapital);
-    while(_startYearCapital > 0) {
-            int counter = _iterations;
-        for (int i = _yearsForCalc; i < 2021; i++) {
-                _endYearCapital = _startYearCapital + (_startYearCapital * _moexDelta[counter]);
-                System.out.println("         Конец " + i + " : " + _endYearCapital);
-                _startYearCapital = _endYearCapital - (_getPercent + (Constances.INFLATION_RATE[counter-1]));
-                System.out.println(" Начало " + (i + 1) + " : " + _startYearCapital);
-            counter++;
+        if (_yearsForCalc > 2021){
+            return;
         }
-        _getPercent += 0.5;
-        System.out.println(_getPercent);
-    }
-    _getPercent -= 0.5;
-        System.out.println(_getPercent);
-    }
+        System.out.println ("Максимальный процент изъятия : " + GetPercentCalculations(_startYearCapital,_endYearCapital, _yearsAmount, _initialCapital, _getPercent, _yearsForCalc, MoexDelta()));
 
+    }
+   public static float GetPercentCalculations(double startYearCapital, double endYearCapital, int yearsAmount, int initialCapital, float getPercent,  int yearsForCalc, double[]moexDelta ){
+
+        while (startYearCapital > 0) {
+            int counter = (Constances.INFLATION_RATE.length - yearsAmount);
+            startYearCapital = initialCapital - getPercent;
+            endYearCapital = 0;
+
+            for (int i = yearsForCalc; i < 2022; i++) {
+                System.out.println(i + " - " + startYearCapital);
+                endYearCapital = startYearCapital + (startYearCapital * moexDelta[counter]);
+                startYearCapital = endYearCapital - (getPercent + (Constances.INFLATION_RATE[counter - 1]));
+                counter++;
+            }
+                getPercent += 0.5f;
+        }
+        getPercent -= 1f;
+        if(getPercent > 0){
+            return getPercent;
+        } else {
+            return 0;
+        }
+    }
     //вычисление дельты мосБиржи
     public static double [] MoexDelta(){
         double [] moexDelta = new double[21];
